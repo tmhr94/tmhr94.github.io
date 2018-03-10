2b0bceb1128fbd32b690e8b11b13e30e1c59cb6d
rikoroku <v2gats213@gmail.com>
2018-03-10 21:01:28 +0900

    [add] getJobExperiences

--- github_io/src/providers/firebase-service/firebase-service.ts
+++ github_io/src/providers/firebase-service/firebase-service.ts
@@ -1,11 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class FirebaseServiceProvider {
+
+  public url: string;
 
   constructor(public http: HttpClient) {
     this.url = 'https://github-io-9a43a.firebaseio.com/';
   }
 
+  getJobExperiences(): Observable<any> {
+    return this.http.get(this.url + 'job_experiences.json').map(
+      res => { return res; }
+    );
+  }
 }
