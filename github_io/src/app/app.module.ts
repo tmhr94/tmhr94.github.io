9034da7c7079d066c30f9380a937aa9b4c7d9299
rikoroku <v2gats213@gmail.com>
2018-03-10 17:16:50 +0900

    [add] HttpClientModule

--- github_io/src/app/app.module.ts
+++ github_io/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MyApp } from './app.component';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { FirebaseServiceProvider } from '../providers/firebase-service/firebase-service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FirebaseServiceProvider } from '../providers/firebase-service/firebase-
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
